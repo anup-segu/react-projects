c2396f25053432a71a94497b25f0309abfb606d4
anup-segu <anup.segu@gmail.com>
2016-04-22 14:20:19 -0700

    Add map element

--- frontend/components/map.jsx
+++ frontend/components/map.jsx
@@ -1,8 +1,22 @@
 var React = require('react');
 
 var Map = React.createClass({
+  // APIKEY: AIzaSyA--QaFNrxg26Vhlu8aPbHGKyqKf3_42C4
+  componentDidMount: function(){
+      var mapDOMNode = this.refs.map;
+      var mapOptions = {
+        center: {lat: 37.7758, lng: -122.435},
+        zoom: 13
+      };
+      this.map = new google.maps.Map(mapDOMNode, mapOptions);
+    },
+
   render: function(){
-    
+    return (
+      <div className="map" ref="map">
+
+      </div>
+    );
   }
 });
 
